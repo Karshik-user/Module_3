df4be758508e5e5f7791a604dd158dbfad77f832
userThe_Karshik <kurasov86@mai.ru>
2024-09-13 20:17:41 +0400

    Д/З от 13,09,2024г

--- Module_3_1.py
+++ Module_3_1.py
@@ -1,1 +1,25 @@
-# Дом Зад
+#Домашняя работа по уроку "Пространство имён"
+
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return {len(string),string.upper(),string.lower(),}
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return (string.lower()
+            in (item.lower()
+                for item in list_to_search))
+
+info = string_info
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+
+result = is_contains
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print("Количество вызовов функций:", calls)
